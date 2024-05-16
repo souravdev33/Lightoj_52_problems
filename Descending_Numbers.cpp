e4d2e2d8f57521c51ff82dec77603b3b44f0008b
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-05-16 09:54:32 +0600

    Add files via upload

---
+++ Descending_Numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i,j=0;
+    for(i=1000;i>=1;i--){
+        cout<<i<<' ';
+        j++;
+        if(j%5==0)cout<<endl;
+       
+    }
+return 0;
+}
