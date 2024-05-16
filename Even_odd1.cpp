f3f82d114201ec8d49c239a26a21b03ca24fcd87
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-05-16 09:02:32 +0600

    Even_odd1.cpp

---
+++ Even_odd1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void boom() {
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n;
+        cin>>n;
+        if(n%2==0)cout<<"even"<<endl;
+        else cout<<"odd"<<endl;
+    }
+}
+
+int main() {
+      
+    boom();
+    return 0;
+}
